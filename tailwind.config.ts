560a3ddb5297818b7e0cb62ed382b12d80d92dc6
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2024-05-01 19:32:53 +0200

    initialice project
    - add prettier-tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		letterSpacing: {
+			tightest: '-.2em',
+			tighter: '-.25em',
+			tight: '-.33em',
+			normal: '0',
+		},
+		extend: {
+			colors: {
+				softblue: {
+					100: '#62bcfa',
+					200: '#4661E6',
+				},
+				grayblue: {
+					100: '##647196',
+					200: '##3A4374',
+					300: '#373F68',
+				},
+				gray: {
+					100: '#F7F8FD',
+					200: '#F2F4FF',
+				},
+				violet: '#AD1FEA',
+				orange: '#F49F85',
+				white: '#FFFFFF',
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
